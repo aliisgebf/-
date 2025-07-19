6dfc2ced29f8988fda7843839a560c2ad903d77d
aliisgebf <aliix295@gmail.com>
2025-07-19 03:56:30 +0300

    Delete controllers directory

--- controllers/routes/sessions.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const sessionController = require('../controllers/sessionController');
-
-router.post('/', sessionController.createSession);
-router.get('/', sessionController.getAllSessions);
-
-module.exports = router;
